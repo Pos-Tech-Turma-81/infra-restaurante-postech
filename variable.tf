2547756dd54c5c9128c17ce8599c65e94e19c148
Marcelo Benjamim <mah.benjamim12@gmail.com>
2025-09-15 08:38:49 -0300

    criando o EKS

--- variable.tf
+++ variable.tf
@@ -3,3 +3,35 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1" # pode mudar aqui
 }
+
+variable "regionDefault" {
+  default = "us-east-1"
+}
+
+variable "projectName" {
+  default = "EKS-FIAP"
+}
+
+variable "labRole" {
+  default = "arn:aws:iam::368058795395:role/LabRole"
+}
+
+variable "accessConfig" {
+  default = "API_AND_CONFIG_MAP"
+}
+
+variable "nodeGroup" {
+  default = "fiap"
+}
+
+variable "instanceType" {
+  default = "t3.medium"
+}
+
+variable "principalArn" {
+  default = "arn:aws:iam::368058795395:role/voclabs"
+}
+
+variable "policyArn" {
+  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
