4a434338f39764028825470709284275d76b2e9c
Mabel <mabelus21@icloud.com>
2025-09-30 21:47:37 -0300

    Update S3 bucket name for Terraform state

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-turma-82"    # seu bucket
+    bucket         = "my-terraform-state-turma-83"    # seu bucket
     key            = "infra-eks.tfstate"     # caminho do state no S3
     region         = "us-east-1"             # região do bucket
   }
