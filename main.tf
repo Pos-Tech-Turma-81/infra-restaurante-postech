35bc871593cc24aa3cda93e18397327eed3fb784
Felipe "sittaman" Sitta <musicplant@gmail.com>
2026-03-09 23:36:02 -0300

    bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "state-turma-postech-198"    # seu bucket
+    bucket         = "state-postech-infra-199"       # seu bucket
     key            = "infra-eks.tfstate"     # caminho do state no S3
     region         = "us-east-1"             # região do bucket
   }
